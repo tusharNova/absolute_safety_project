da3932897a13c73be1a3930980b283ef1f0dc505
tusharNova <tusharmankar02@gmail.com>
2025-07-15 19:05:33 +0530

    clinet request form

--- webapp/dashboard/Create-request.php
+++ webapp/dashboard/Create-request.php
@@ -320,8 +320,8 @@
 
             <div class="container-xxl flex-grow-1 container-p-y">
               <h4 class="fw-bold py-3 mb-4">
-                <span class="text-muted fw-light">Account Settings /</span>
-                Account
+                <span class="text-muted fw-light">Create Request For Certificate /</span>
+                
               </h4>
 
               <div class="row">
@@ -329,70 +329,13 @@
                   <ul class="nav nav-pills flex-column flex-md-row mb-3">
                     <li class="nav-item">
                       <a class="nav-link active" href="javascript:void(0);"
-                        ><i class="bx bx-user me-1"></i> Account</a
+                        ><i class="bx bx-user me-1"></i> </a
                       >
                     </li>
-                    <li class="nav-item">
-                      <a
-                        class="nav-link"
-                        href="pages-account-settings-notifications.html"
-                        ><i class="bx bx-bell me-1"></i> Notifications</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a
-                        class="nav-link"
-                        href="pages-account-settings-connections.html"
-                        ><i class="bx bx-link-alt me-1"></i> Connections</a
-                      >
-                    </li>
+                    
                   </ul>
                   <div class="card mb-4">
-                    <h5 class="card-header">Profile Details</h5>
-                    <!-- Account -->
-                    <div class="card-body">
-                      <div
-                        class="d-flex align-items-start align-items-sm-center gap-4"
-                      >
-                        <img
-                          src="./assets/img/avatars/1.png"
-                          alt="user-avatar"
-                          class="d-block rounded"
-                          height="100"
-                          width="100"
-                          id="uploadedAvatar"
-                        />
-                        <div class="button-wrapper">
-                          <label
-                            for="upload"
-                            class="btn btn-primary me-2 mb-4"
-                            tabindex="0"
-                          >
-                            <span class="d-none d-sm-block"
-                              >Upload new photo</span
-                            >
-                            <i class="bx bx-upload d-block d-sm-none"></i>
-                            <input
-                              type="file"
-                              id="upload"
-                              class="account-file-input"
-                              hidden
-                              accept="image/png, image/jpeg"
-                            />
-                          </label>
-                          <button
-                            type="button"
-                            class="btn btn-outline-secondary account-image-reset mb-4"
-                          >
-                            <i class="bx bx-reset d-block d-sm-none"></i>
-                            <span class="d-none d-sm-block">Reset</span>
-                          </button>
-
-                          <p class="text-muted mb-0">
-                            Allowed JPG, GIF or PNG. Max size of 800K
-                          </p>
-                        </div>
-                      </div>
+                    
                     </div>
                     <hr class="my-0" />
                     <div class="card-body">
@@ -404,27 +347,27 @@
                         <div class="row">
                           <div class="mb-3 col-md-6">
                             <label for="firstName" class="form-label"
-                              >First Name</label
+                              >Mechine Name</label
                             >
                             <input
                               class="form-control"
                               type="text"
                               id="firstName"
                               name="firstName"
-                              value="John"
+                              value=""
                               autofocus
                             />
                           </div>
                           <div class="mb-3 col-md-6">
                             <label for="lastName" class="form-label"
-                              >Last Name</label
+                              >machine Detail</label
                             >
                             <input
                               class="form-control"
                               type="text"
                               name="lastName"
                               id="lastName"
-                              value="Doe"
+                              value=""
                             />
                           </div>
                           <div class="mb-3 col-md-6">
@@ -435,7 +378,7 @@
                               id="email"
                               name="email"
                               value="john.doe@example.com"
-                              placeholder="john.doe@example.com"
+                              placeholder="doe@example.com"
                             />
                           </div>
                           <div class="mb-3 col-md-6">
@@ -514,6 +457,9 @@
                           >
                             Cancel
                           </button>
+                          <button type="submit" class="btn btn-primary me-2">
+                            QR
+                          </button>
                         </div>
                       </form>
                     </div>
@@ -565,7 +511,7 @@
             <!-- / Content -->
 
             <!-- Footer -->
-            <!-- Footer -->
+       
             <footer class="content-footer footer bg-footer-theme">
               <div
                 class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column"
@@ -581,7 +527,7 @@
               </div>
             </footer>
             <!-- / Footer -->
-            <!-- / Footer -->
+           
 
             <div class="content-backdrop fade"></div>
           </div>
