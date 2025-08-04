9c8c7b3e5171e389a3aa6991352df54101738800
tusharNova <tusharmankar02@gmail.com>
2025-08-04 14:30:55 +0530

    add more session for demo

--- webapp/auth/session.php
+++ webapp/auth/session.php
@@ -12,11 +12,24 @@ if (isset($_SESSION['user'])) {
     echo json_encode(['status' => 'not_logged_in']);
 }
 
-echo "<br><br>";
-
 // $user = $_SESSION['user'];
 // $fullname = $user['first_name'] ." ". $user['last_name'];
 // $uid =$user['id'];
 // echo $fullname;
 // echo "<br>";
 // echo $uid;
+echo "<br><br>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testing session.php</title>
+</head>
+<body>
+    
+</body>
+</html>
+
